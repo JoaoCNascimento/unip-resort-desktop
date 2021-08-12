8f8af00fb491accab44555bfc0073f83deb8f92a
João Nascimento <joao.nascimento0576@gmail.com>
2021-08-12 20:04:15 -0300

    Alteração de nome de classe e criação de classe para conexão com banco de dados.

---
+++ CadastroBasico/Classes/Database.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace GestaoUnipResort.Classes
+{
+    class Database
+    {
+        public SqlConnection con = new SqlConnection();
+
+        public Database ()
+        {
+            con.ConnectionString = @"Data Source=localhost;Initial Catalog=UNIP_RESORT;Integrated Security=True"; 
+        }
+
+        public SqlConnection conectar ()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            return con;
+        }   
+
+        public void desconectar ()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
